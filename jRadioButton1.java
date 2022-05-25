782ec4b8fff7c7f71d727930195f5554dab38b22
UMAMAHESHWARRAO302001 <61739299+UMAMAHESHWARRAO302001@users.noreply.github.com>
2022-05-25 21:20:24 +0530

    Add files via upload

---
+++ jRadioButton1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package my_package;
+
+/**
+ *
+ * @author dell
+ */
+class jRadioButton1 {
+    static boolean isSelected() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    static void setSelected(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
